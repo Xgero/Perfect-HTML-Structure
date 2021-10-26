5ab991260104f4cda4ca9edf74f01035c2266103
Xgero <93048758+Xgero@users.noreply.github.com>
2021-10-27 00:16:38 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,32 @@
+<!doctype html>
+<html lang="fr">
+
+	<head>
+	
+		<meta charset="utf-8">
+		<meta name="copyright" content="François Petit">
+		<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
+		
+		<link rel="stylesheet" href="https://xgero.com/assets/css/style.css">
+		
+		<title>XGERO</title>
+		
+	</head>
+
+	<body>
+
+		<nav class="light_grey">
+			<div class="container">
+				<span id="logo" class="strong">XGERO</span>
+			</div>
+		</nav>
+
+		<section>
+			<div class="container">
+				<h1>Perfect HTML Structure</h1>
+			</div>
+		</section>
+		
+	</body>
+
+</html>

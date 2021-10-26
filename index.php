baf4ee99e5e4be02e2b8577cdb83fd5fcb4d5300
Xgero <francois.petit@1984.agency>
2021-10-27 01:30:31 +0200

    Corrections

--- index.php
+++ index.php
@@ -24,6 +24,15 @@
 			<p class="grey">Cette structure web a pour but d’être la plus simple, fiable, compatible et valide avec la plupart des standards web. 
 			Xgero.com est une vitrine fonctionnelle du projet Perfect HTML Structure afin de servir d‘exemple d’utilisation via du contenu réel. </p>
 		</div>
+		
+		<div class="container m_t_xl">
+			<h2>Validation</h2>
+			<ul>
+				<li><a href="https://validator.w3.org/nu/?showoutline=yes&doc=https%3A%2F%2Fxgero.com%2F" title="validator.w3.org">Tester sur validator.w3.org</a></li>
+			</ul>
+		</div>
+
+		
 
 	</body>
 

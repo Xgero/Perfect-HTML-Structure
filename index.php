0008564c2f9868f426f34c27bb014a1eb49d83a2
Xgero <francois.petit@1984.agency>
2021-10-27 00:50:09 +0200

    Corrections

--- index.php
+++ index.php
@@ -2,13 +2,13 @@
 <html lang="fr">
 
 	<head>
-	
+
 		<meta charset="utf-8">
 		<meta name="copyright" content="François Petit">
-		<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
-		
+		<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+
 		<link rel="stylesheet" href="https://xgero.com/assets/css/style.css">
-		
+
 		<title>XGERO</title>
 		
 	</head>
@@ -24,6 +24,7 @@
 		<section>
 			<div class="container">
 				<h1>Perfect HTML Structure</h1>
+				<p class="grey">Cette structure web a pour but d’être la plus simple, fiable, compatible et valide avec la plupart des standards web. Xgero.com est une vitrine fonctionnelle de Perfect HTML Structure afin de servir d‘exemple d’utilisation via du contenu réel. </p>
 			</div>
 		</section>
 		

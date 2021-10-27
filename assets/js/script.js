43ef52e217e5f0654ad555ce4ec1860e95d0f4e4
Xgero <francois.petit@1984.agency>
2021-10-28 00:48:30 +0200

    Corrections

---
+++ assets/js/script.js
@@ -0,0 +1,6 @@
+
+/********** GOOGLE ANALYTICS ***********/
+window.dataLayer = window.dataLayer || [];
+function gtag(){dataLayer.push(arguments);}
+gtag('js', new Date());
+gtag('config', 'G-S31SK3HTSH');
